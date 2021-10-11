b7a5ba55ff5ffabb09d0f344b7b3b13c2186e44a
itstimiking <26443032+itstimiking@users.noreply.github.com>
2021-10-11 22:59:43 +0100

    Permissions.Location

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,5 @@
 import React from "react";
-import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location';
 import * as Notifications from 'expo-notifications';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -38,39 +38,37 @@ export const clearLocalNotifications = () => {
     );
 };
 
-export const setLocalNotification = () => {
+export const setLocalNotification = async () => {
     AsyncStorage.getItem(NOTIFICATION_KEY_MOBILE_FLASHCARD)
         .then(JSON.parse)
         .then((data) => {
             if (data === null) {
-                Permissions.askAsync(Permissions.NOTIFICATIONS).then(
-                    (status) => {
-                        if (status === "granted") {
-                            Notifications.cancelAllScheduledNotificationsAsync();
+                
+                let { status } = await Location.requestForegroundPermissionsAsync();
+                if (status === 'granted') {
+                    Notifications.cancelAllScheduledNotificationsAsync();
 
-                            let tomorrow = new Date();
-                            tomorrow.setDate(tomorrow.getDate() + 1);
-                            tomorrow.setHours(7);
-                            tomorrow.setMinutes(0);
+                    let tomorrow = new Date();
+                    tomorrow.setDate(tomorrow.getDate() + 1);
+                    tomorrow.setHours(7);
+                    tomorrow.setMinutes(0);
 
-                            Notifications.scheduleNotificationAsync({
-                                content: {
-                                    title: "Mobile flashcards app",
-                                    body: " Dont forget to practice for your upcomming test today",
-                                },
-                                trigger: {
-                                    seconds: tomorrow,
-                                    repeats: true,
-                                },
-                            });
+                    Notifications.scheduleNotificationAsync({
+                        content: {
+                            title: "Mobile flashcards app",
+                            body: " Dont forget to practice for your upcomming test today",
+                        },
+                        trigger: {
+                            seconds: tomorrow,
+                            repeats: true,
+                        },
+                    });
 
-                            AsyncStorage.setItem(
-                                NOTIFICATION_KEY_MOBILE_FLASHCARD,
-                                JSON.stringify(true)
-                            );
-                        }
-                    }
-                );
+                    AsyncStorage.setItem(
+                        NOTIFICATION_KEY_MOBILE_FLASHCARD,
+                        JSON.stringify(true)
+                    );
+                }
             }
         })
         .catch(err=>console.log(err))
